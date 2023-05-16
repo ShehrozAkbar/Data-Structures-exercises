758a751c473cbe138b1fd9677d3b19dbca2ef37d
Shehroz Akbar <113382674+ShehrozAkbar@users.noreply.github.com>
2023-05-16 14:58:34 +0500

    Update script13sorting.js

--- script13sorting.js
+++ script13sorting.js
@@ -38,7 +38,7 @@ function selectSort(arr) {
 
 // insertion sort (youtube solution)
 // Javascript Freecodecamp Algorithm #33: Implement Insertion Sort
-let array = [21, 19, 14, 21, 36, 54, 100, 26, 40];
+// let array = [21, 19, 14, 21, 36, 54, 100, 26, 40];
 
 function insertSort(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -55,7 +55,7 @@ function insertSort(arr) {
   return arr;
 }
 
-console.log(insertSort(array));
+// console.log(insertSort(array));
 
 // these both sorting algorithms use divide and qunquure technique to sort thungs out
 
@@ -63,3 +63,37 @@ console.log(insertSort(array));
 
 // quick sort (we have to find the pivot init and place the small indexes to the left and the large indexes to the right of the pivot)
 // Note: pivot here is the random value or index which you choose.
+
+// merge sort solution
+function mergeSort(arr) {
+  // we have to use recursion in it
+  const len = arr.length;
+  const half = Math.floor(len / 2);
+  const left = arr.slice(0, half);
+  const right = arr.slice(half);
+
+  // console.log(half, left, right);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  console.log(".this is left: " + left + ".this is right: " + right);
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+let array = [21, 19, 14, 21, 36, 54, 100, 26, 40];
+
+mergeSort(array);
